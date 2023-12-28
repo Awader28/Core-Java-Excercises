a9475c328b318323a5184e179c86811fecb62d2c
Atharva Wader <awader28@gmail.com>
2023-12-28 14:47:37 +0530

    Commit

--- src/com/internship/day07/exception/dbconn/Demo.java
+++ src/com/internship/day07/exception/dbconn/Demo.java
@@ -3,33 +3,40 @@ package com.internship.day07.exception.dbconn;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Demo {
-public static void main(String[] args) {
-	String driver="com.mysql.jdbc.Driver";
-	String url="jdbc:mysql://localhost:3306/avion?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
-	String user="root";
-	String pass="atharva28";
-	
-	try {
-	  Class.forName(driver);
-	  Connection Con=DriverManager.getConnection(url,user,pass);
-	  System.out.println("CONNECTION SUCCESSFUL");
-	} catch (ClassNotFoundException e1) {
-	    // Handle ExceptionType1
-	    // ...
-		System.out.println("DATABASE DRIVER NOT FOUND");
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		System.out.println("SQL RELATED EXCEPTIONN CHECK USERID, PASSWORD AND URL");
-	}
-}
-	
+    public static void main(String[] args) {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/avion?useSSL=false&serverTimezone=UTC&useLegacyDatetimeCode=false";
+        String user = "root";
+        String pass = "atharva28";
+        //String insert = "INSERT INTO `internship`.`tblemployee` (`name`, `city`, `salary`) VALUES ('Atharva', 'Palghar', '12354');";
+        String insert2 = "INSERT INTO `internship`.`tblemployee` (`name`, `city`, `salary`) VALUES ('Soshor', 'Popoe', '12355984');";
 
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, user, pass);
+            System.out.println("CONNECTION SUCCESSFUL");
 
+            // Create a statement and execute the insert query
+            Statement statement = con.createStatement();
+            int rowsAffected = statement.executeUpdate(insert2);
+            statement.executeUpdate(insert2);
+            if (rowsAffected > 0) {
+                System.out.println("Insertion successful");
+            } else {
+                System.out.println("Insertion failed");
+            }
 
+            // Close the connection and statement
+            statement.close();
+            con.close();
 
-
-
-
+        } catch (ClassNotFoundException e1) {
+            System.out.println("DATABASE DRIVER NOT FOUND");
+        } catch (SQLException e) {
+            System.out.println("SQL RELATED EXCEPTION: " + e.getMessage());
+        }
+    }
 }
